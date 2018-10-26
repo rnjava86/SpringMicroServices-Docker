7612d2a2dce2d5a6fbe9ea1c792b94c483e8c58e
Vineeth <34542905+vvyasabhattu@users.noreply.github.com>
2018-10-26 15:56:37 +0530

    Swagger UI Added

--- online-shoping/customer-service/src/main/java/org/evoke/customer/Application.java
+++ online-shoping/customer-service/src/main/java/org/evoke/customer/Application.java
@@ -4,12 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@Import(SwaggerConfig.class)
 @EnableEurekaClient
 @EnableWebMvc
 @SpringBootApplication
 @EnableDiscoveryClient
+@Configuration
 public class Application {
 
     public static void main(String[] args) {
