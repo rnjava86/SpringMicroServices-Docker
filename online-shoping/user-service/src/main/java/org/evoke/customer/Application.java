b6b07e5f5dc40b6ea0778f822fdfedfdb4465b7b
Vineeth <34542905+vvyasabhattu@users.noreply.github.com>
2018-10-30 16:19:42 +0530

    Removed @EnableWebMvc in Application class

--- online-shoping/user-service/src/main/java/org/evoke/customer/Application.java
+++ online-shoping/user-service/src/main/java/org/evoke/customer/Application.java
@@ -6,11 +6,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Import(SwaggerConfig.class)
 @EnableEurekaClient
-@EnableWebMvc
 @SpringBootApplication
 @EnableDiscoveryClient
 @Configuration
